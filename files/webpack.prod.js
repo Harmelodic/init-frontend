1a67dfa9e7765f4d6cca9c05c9a2f896eb2de1e4
Matt Smith <matt@harmelodic.com>
2020-10-24 17:41:07 +0000

    Update webpack.prod.js

--- files/webpack.prod.js
+++ files/webpack.prod.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const config = require('./webpack.config.js');
@@ -8,11 +8,7 @@ module.exports = merge(config, {
   plugins: [
     new MinifyPlugin(),
     new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
     }),
   ],
 });
